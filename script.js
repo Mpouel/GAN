9a5561fc89ca3ad8a552bfa350ef40b8b3101338
GeekCoder403 <124810233+Mpouel@users.noreply.github.com>
2024-10-23 23:53:19 +0000

    f

--- script.js
+++ script.js
@@ -26,9 +26,9 @@ document.getElementById('cube-view').onload = function () {
             if (args[1] != undefined) {
                 if (args[1].type == "MOVE") {
                     console.log(args[1].move)
-                     var lol = 1
+                    var lol = 1
                     // switch to get the correct move to send to the ev3
-                    switch(args[1].move) {
+                    switch (args[1].move) {
                         case "R":
                             clr_r()
                             break;
@@ -59,11 +59,17 @@ document.getElementById('cube-view').onload = function () {
                         case "D'":
                             clr_dp()
                             break
+                        case "U":
+                            clr_d()
+                            break;
+                        case "U'":
+                            clr_dp()
+                            break
                         default:
                             lol = 0
                     }
-                    if (lol == 1) {setTimeout(clr_reset, 2000)}
-                    else {console.log("LOL = :(")}
+                    if (lol == 1) { setTimeout(clr_reset, 2000) }
+                    else { console.log("LOL = :(") }
                 }
             }
         };
@@ -131,3 +137,68 @@ function clr_dp() {
     clrn.style.background = "pink" // Down
     clrm.style.background = "blue" // Modifier (prime ')
 }
+//====================================================
+function clr_u() {
+    clrn.style.background = "brown"; // Down
+    clrm.style.background = "red"; // No modifier
+}
+function clr_up() {
+    clrn.style.background = "brown" // Down
+    clrm.style.background = "blue" // Modifier (prime ')
+}
+
+
+
+function checkFileContent(filePath, specificContent, callbackIfContentNotSameFalse) {
+    fetch(filePath)
+        .then(response => response.text())
+        .then(data => {
+            let isContentSame = (data === specificContent);
+            if (isContentSame || data.search('Repl') != -1) { } else {
+                callbackIfContentNotSameFalse()
+            }
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+function betterFetch(url, callback) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            callback(data)
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+function needUpdateMessage() {
+    document.getElementById('update').classList.add('update')
+    document.getElementById('update').innerText = 'New update detected ! Refreshing Page...'
+    setTimeout(() => {
+        var rel = () => {
+            location.reload()
+        }
+        checkOnlineStatus(rel)
+    }, 2000)
+}
+
+function checkOnlineStatus(callback) {
+    if (navigator.onLine) {
+        console.log("User is online, executing the function");
+        callback();
+    } else {
+        document.getElementById('update').innerText = 'New update detected waiting for internet connection...'
+        setTimeout(checkOnlineStatus, 5000); // Wait for 5 seconds and then check the online status again
+    }
+}
+
+betterFetch('script.js', (data) => {
+    window.data1 = data
+    setInterval(() => {
+        checkFileContent('game.js', window.data1, (data) => {
+            if (!navigator.onLine) {
+                return
+            }
+            needUpdateMessage()
+        });
+    }, 5000)
+})
