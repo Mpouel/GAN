24a5091d436570c0685c3ea0a45be07943344e87
GeekCoder403 <124810233+Mpouel@users.noreply.github.com>
2024-10-29 16:47:47 -0400

    s

--- gan/copy_mac.js
+++ gan/copy_mac.js
@@ -1,5 +1,5 @@
 function copy_mac() {
-    var mac = document.getElementById("mac");
+    var mac = document.getElementById("mac").value
     mac.select();
     mac.setSelectionRange(0, 99999);
     navigator.clipboard.writeText(mac.innerText);
