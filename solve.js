dd024c6234233c011979ff871d6b11d94d19feae
GeekCoder403 <124810233+Mpouel@users.noreply.github.com>
2024-10-29 11:42:01 -0400

    d

--- solve.js
+++ solve.js
@@ -54,8 +54,9 @@ let rmoves = moves.reverse()
 function send() {
     rmoves = moves.reverse()
     send_signal()
-    sleep(1000).then(() => { clr_reset(); });
-    sleep(1000).then(() => { send_ev3(); })
+    sleep(1000)
+        .then(() => { clr_reset() })
+        .then(() => { sleep(1000).then(() => { send_ev3() }) });
 }
 
 function send_ev3() {
