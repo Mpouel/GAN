08b31bfb36d4e89660f2b3409602c752ce73ffa1
Gilaxy04 <79372051+franck403@users.noreply.github.com>
2024-11-05 08:08:50 -0500

    Create oldconsole.js

---
+++ oldconsole.js
@@ -0,0 +1,78 @@
+import { Peer } from "https://esm.sh/peerjs@1.5.4?bundle-deps";
+
+var peer = "lol"
+if (localStorage.getItem('sharedConsole') == 'server') {
+    var peer = new Peer('ganrobotconsole');
+} else if (localStorage.getItem('sharedConsole') == 'client') {
+    var peer = new Peer();
+}
+var oldlog = console.log
+var olderror = console.error
+  
+peer.on('open', (id) => {
+    console.log('open')
+    // Connect to another peer
+
+    if (localStorage.getItem('sharedConsole') == 'server') {
+        peer.on('connection', (conn) => {
+            conn.on('data', (data) => {
+                console.log(data)
+            });
+        });
+    } else if (localStorage.getItem('sharedConsole') == 'client') {
+        const peerId = 'ganrobotconsole';
+        const conn = peer.connect(peerId);
+
+        conn.on('open', () => {
+            displayMessage('Connected to peer: ' + peerId, 'log');
+            conn.send('Connected to console sharing');
+        });
+        function compute(...args) {
+            var ret = []
+            args.forEach(element => {
+                if (typeof element === "string") {
+                    var content = {
+                        strings: element
+                    };
+                }
+                if (typeof element === "number") {
+                    var content = {
+                        numbers: element
+                    };
+                }
+                if (Array.isArray(element)) {
+                    var content = {
+                        arrays: element
+                    };
+                }
+                ret.push(content)
+            });
+            return ret
+        }
+        console.log = function (...args) {
+            console.log(args)
+            args.forEach(element => {
+                conn.send(compute(element))
+            });
+        }
+        console.error = function (...args) {
+            console.log(args)
+            conn.send(args.toString())
+        }
+        conn.on('data', (data) => {
+            displayMessage(message)
+        });
+    }
+
+    function displayMessage(message, type) {
+
+        // Log to console based on type
+        if (type === 'log') {
+            oldlog(message);
+        } else if (type === 'error') {
+            olderror(message);
+        } else {
+          oldlog(message)
+        }
+    }
+});
