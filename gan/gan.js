c31722f270c4a75d6e5402d0d9dfe4a8856515db
Mpouel <gauthierc2@cssdd.ca>
2025-09-05 20:08:56 -0400

    xs

---
+++ gan/gan.js
@@ -0,0 +1,10 @@
+console.log = function (...args) {
+    if (false) {
+        const facelets = args[1].facelets;
+        console.log(facelets);
+    }
+}
+
+function reset() {
+    console.log('Reset state')
+}
